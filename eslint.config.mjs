bbdf590b738c9a43068b7cb02c83709c547ba6cd
Angel Lopez <srterradev@gmail.com>
2025-12-05 23:26:46 -0600

    chore: eslint rules update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const eslintConfig = defineConfig([
     'build/**',
     'next-env.d.ts',
   ]),
+  {
+    rules: {
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
 ])
 
 export default eslintConfig
