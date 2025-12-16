1d654be0cc63f9a6e025c3d555247f422fff8b82
Angel Lopez <srterradev@gmail.com>
2025-12-15 20:34:30 -0600

    feat: eslint config file changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,27 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts", "node_modules/**"],
+  },
   {
     rules: {
       "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+      "no-unused-vars": "off",
+      "no-unused-private-class-members": "warn",
+      "no-unreachable": "warn",
+      "no-unused-expressions": "warn",
     },
   },
 ]);
